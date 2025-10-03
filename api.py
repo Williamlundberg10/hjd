b5de87659b879fee8a7b94a2619ed2ce2f4e64de
Williamlundberg10 <williamlundberg10.home@gmail.com>
2025-10-03 13:21:25 +0200

    lli

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/h")
+def home():
+    return "Hello, Flask on GitHub Pages (well... sort of)! 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
